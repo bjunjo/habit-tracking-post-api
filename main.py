e85ce156f455bba598c1f02f05fa138e116a1a67
byoungjunjo <byoungjunjo@gmail.com>
2021-05-07 18:20:33 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,64 @@
+import requests
+from datetime import *
+
+TOKEN = "CREATE YOUR OWN TOKEN"
+PIXELA_ENDPOINT = "https://pixe.la/v1/users"
+USERNAME = "CREATE YOUR ID"
+GRAPH_ID = "graph0" # Create any graph ID
+graph_endpoint = f"{PIXELA_ENDPOINT}/{USERNAME}/graphs"
+get_graph_endpoint = f"{graph_endpoint}/{GRAPH_ID}"
+
+ask_quantity = input("How many km did you walk today? ")
+today = datetime.now()
+date = today.strftime("%Y%m%d")
+
+headers = {
+    "X-USER-TOKEN": TOKEN,
+}
+
+# TODO 1.Create a user ID
+params = {
+    "token": TOKEN,
+    "username": USERNAME,
+    "agreeTermsOfService":"yes",
+    "notMinor": "yes",
+}
+
+response = requests.post(PIXELA_ENDPOINT, json=params)
+
+# TODO 2. Create a graph
+graph_endpoint = f"{PIXELA_ENDPOINT}/{USERNAME}/graphs"
+
+graph_params = {
+    "id": GRAPH_ID,
+    "name": "Walking Graph",
+    "unit": "Km",
+    "type": "float",
+    "color": "shibafu",
+}
+
+response = requests.post(url=graph_endpoint, json=graph_params, headers=headers)
+
+# TODO 3. Get the graph
+print(get_graph_endpoint)
+print(response.text)
+
+# TODO 4. Post a pixel
+pixel_params = {
+    "date": date,
+    "quantity": ask_quantity,
+}
+
+response = requests.post(url=get_graph_endpoint, json=pixel_params, headers=headers)
+
+# TODO 5. Update a pixel.
+update_endpoint = f"{get_graph_endpoint}/{date}"
+update_pixel_params = {
+    "quantity": ask_quantity,
+}
+
+response = requests.put(url=update_endpoint, json=update_pixel_params, headers=headers)
+
+# TODO 6. Delete a pixel
+delete_endpoint = f"{get_graph_endpoint}/{date}"
+response = requests.delete(url=delete_endpoint, headers=headers)
